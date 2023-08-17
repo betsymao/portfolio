2ad3957a45da06e4713a61bc29b0df44639076d6
Betsy <wdbetsy@gmail.com>
2023-08-17 19:40:10 +1000

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './sections/Header';
+import About from './sections/About';
+import Projects from './sections/Projects';
+import Footer from './sections/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <About />
+      <Projects />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
